6fcb4d066916fd6e6ae8403a026d41f372a4cfef
kevin <2587257782@qq.com>
2018-09-22 17:58:58 +0800

    save the data to mongodb(v 4.02),not finished

--- pipelines.py
+++ pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy.conf import settings
+import pymongo
 
 class SinanbaspiderPipeline(object):
+	def __init__(self):
+		host = settings['MONGODB_HOST']
+		port = settings['MONGODB_PORT']
+		dbname = setting['MONGODB_DBNAME']
+
+		# 建立数据库连接
+		client = pymongo.MongoCLient(host=host,port=port)
+		# 指向指定数据库   mdb = client['Douban']
+		mdb = dbname
+		# 表名
+		self.post = mdb[settings['MONGODB_DOCNAME']]
+
     def process_item(self, item, spider):
+    	data = dict(item)
+    	self.post.insert(data)
         return item
--- settings.py
+++ settings.py
@@ -65,9 +65,14 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'sinaNBASpider.pipelines.SinanbaspiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'sinaNBASpider.pipelines.SinanbaspiderPipeline': 300,
+}
+MONGODB_HOST = '127.0.0.1'
+MONGODB_PORT = 27017
+MONGODB_DBNAME = 'sinaNBA'
+MONGODB_DOCNAME = 'testData'
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
